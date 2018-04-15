c5fa08121fe8d9bc70044545a2d3202a085668b6
Nepcam <cameron.nepe@gmail.com>
2018-04-15 20:06:33 +1200

    Chess Board

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,7 +1,7 @@
 // Looping Triangle
 for (let line = "#"; line.length < 8; line += "#")
   console.log(line);
-  
+
 
   // Fizz Buzz
   for (let n = 1; n <= 100; n++) {
@@ -10,3 +10,21 @@ for (let line = "#"; line.length < 8; line += "#")
     if (n % 5 == 0) output += "Buzz";
     console.log(output || n);
   }
+
+// Chess Board
+  let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);// Your code here.
